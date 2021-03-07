68cbc553376420d454261c4fee135af894572c1f
devadminsam <netengineersam96@gmail.com>
2021-03-07 08:44:49 +0300

    my notification code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!!'));
+app.get('/', (req, res) => res.send('Hello World!! This will trigger a CloudWatch Alarm and send SNS notification'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
